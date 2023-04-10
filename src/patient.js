f3bad0099d1df9904fdb13c4eb5e5776c64a9465
David Scott <sirdavidscott@hotmail.com>
2023-04-10 12:31:53 +0100

    patient class test 2 pass

--- src/patient.js
+++ src/patient.js
@@ -1,7 +1,11 @@
 class Patient {
 
-  constructor() {
-
+  constructor(data) {
+    this.birthDate = data.birthDate;
+    this.zipCode = data.zipCode;
+    this.admissionDate = data.admissionDate;
+    this.dischargeDate = data.dischargeDate;
+    this.notes = data.notes;
   }
 };
 
--- src/patient.test.js
+++ src/patient.test.js
@@ -4,6 +4,24 @@ describe('Patient', () => {
   it('should be defined', () => {
     expect(Patient).toBeDefined();
   });
+
+  describe('constructor', () => {
+    it('should create a patient object when given valid input data', () =>{
+      const validPatientData = {
+        birthDate: "2000-01-01",
+        zipCode: "10013",
+        admissionDate: "2019-03-12",
+        dischargeDate: "2019-03-14",
+        notes: "Patient with ssn 123-45-6789 previously presented under different ssn"
+      };
+      const newPatient = new Patient(validPatientData);
+      expect(newPatient.birthDate).toBe("2000-01-01")
+      expect(newPatient.zipCode).toBe("10013")
+      expect(newPatient.admissionDate).toBe("2019-03-12")
+      expect(newPatient.dischargeDate).toBe("2019-03-14")
+      expect(newPatient.notes).toBe("Patient with ssn 123-45-6789 previously presented under different ssn")
+    })
+  })
 });
 
 
