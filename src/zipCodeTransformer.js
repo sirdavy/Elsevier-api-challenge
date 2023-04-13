62199a1963e6130a45177a160051dbf46753a317
David Scott <sirdavidscott@hotmail.com>
2023-04-13 19:15:03 +0100

    zipcodetransformer second test pass

--- src/zipCodeTransformer.js
+++ src/zipCodeTransformer.js
@@ -9,6 +9,36 @@ class ZipCodeTransformer {
     return parsedData.data;
   }
 
+  transform(zipCode) {
+    const populationByPrefix = {}; // population data for each 3-digit prefix
+    const zipCodeArray = zipCode.split(''); // split the zip code into individual digits
+    const allZips = this.readCsvFile()
+    // Sum the population of all zip codes with the same 3-digit prefix
+    console.log(allZips[1].split);
+    allZips.forEach((row) => {
+      const [zip, population] = row;
+      const prefix = zip.substring(0, 3);
+  
+      if (!populationByPrefix[prefix]) {
+        populationByPrefix[prefix] = 0;
+      }
+  
+      populationByPrefix[prefix] += parseInt(population);
+    });
+  
+    const prefix = zipCodeArray.slice(0, 3).join('');
+    const population = populationByPrefix[prefix] || 0;
+  
+    // If the population is less than 20,000 for this prefix, set the zip code to 00000
+    if (population < 20000) {
+      return '00000';
+    } else {
+      return prefix + '00';
+    }
+  }
+
 }
 
 module.exports = ZipCodeTransformer;
+
+
--- src/zipCodeTransformer.test.js
+++ src/zipCodeTransformer.test.js
@@ -11,8 +11,12 @@ describe("ZipCodeTransformer", () => {
     const transformZip = new ZipCodeTransformer;
     data = transformZip.readCsvFile();
     expect(data[0]).toStrictEqual([ 'Zip Code ZCTA', '2010 Census Population' ]);
-    expect(data[1]).toStrictEqual([ '01001', '16769' ]);
-  
+    expect(data[1]).toStrictEqual([ '01001', '16769' ]);  
+  });
+
+  it("should transform a zipcode's last two digits to '00' when all zipcode populations with the first three digits sum to > 20,000", () => {
+    const transformZip = new ZipCodeTransformer;
+    expect(transformZip.transform("10013")).toBe("10000");
   });
 
 
